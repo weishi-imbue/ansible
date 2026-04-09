09b63b992144daaabce2adb31b8e042c9609b3a6
Weishi Zeng <weishi@imbue.com>
2026-04-09 01:25:12 -0700

    impl patch (16/17)

--- lib/ansible/modules/apt_repository.py
+++ lib/ansible/modules/apt_repository.py
@@ -153,6 +153,7 @@ except ImportError:
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
 from ansible.module_utils.urls import fetch_url
+from ansible.module_utils.common.respawn import has_respawned, respawn_module, probe_interpreters_for_module
 
 
 if sys.version_info[0] < 3:
@@ -184,7 +185,8 @@ def install_python_apt(module):
             else:
                 module.fail_json(msg="Failed to auto-install %s. Error was: '%s'" % (PYTHON_APT, se.strip()))
     else:
-        module.fail_json(msg="%s must be installed to use check mode" % PYTHON_APT)
+        module.fail_json(msg="%s must be installed to use check mode. "
+                             "If run normally this module can auto-install it." % PYTHON_APT)
 
 
 class InvalidSource(Exception):
@@ -552,10 +554,16 @@ def main():
     sourceslist = None
 
     if not HAVE_PYTHON_APT:
+        if not has_respawned():
+            interpreter = probe_interpreters_for_module(
+                ['/usr/bin/python3', '/usr/bin/python2', '/usr/bin/python'], 'apt')
+            if interpreter:
+                respawn_module(interpreter)
         if params['install_python_apt']:
             install_python_apt(module)
         else:
-            module.fail_json(msg='%s is not installed, and install_python_apt is False' % PYTHON_APT)
+            if not HAVE_PYTHON_APT:
+                module.fail_json(msg="{0} must be installed and visible from {1}.".format(PYTHON_APT, sys.executable))
 
     if not repo:
         module.fail_json(msg='Please set argument \'repo\' to a non-empty value')
--- lib/ansible/modules/dnf.py
+++ lib/ansible/modules/dnf.py
@@ -342,6 +342,7 @@ from distutils.version import LooseVersion
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.yumdnf import YumDnf, yumdnf_argument_spec
+from ansible.module_utils.common.respawn import has_respawned, respawn_module, probe_interpreters_for_module
 
 
 class DnfModule(YumDnf):
@@ -515,6 +516,12 @@ class DnfModule(YumDnf):
             else:
                 package = 'python3-dnf'
 
+            candidate_interpreters = ['/usr/libexec/platform-python', '/usr/bin/python3', '/usr/bin/python2', '/usr/bin/python']
+            if not has_respawned():
+                interpreter = probe_interpreters_for_module(candidate_interpreters, 'dnf')
+                if interpreter:
+                    respawn_module(interpreter)
+
             if self.module.check_mode:
                 self.module.fail_json(
                     msg="`{0}` is not installed, but it is required"
@@ -534,9 +541,11 @@ class DnfModule(YumDnf):
             except ImportError:
                 self.module.fail_json(
                     msg="Could not import the dnf python module using {0} ({1}). "
-                        "Please install `{2}` package or ensure you have specified the "
-                        "correct ansible_python_interpreter.".format(sys.executable, sys.version.replace('\n', ''),
-                                                                     package),
+                        "Please install `python3-dnf` or `python2-dnf` package or ensure you have specified the "
+                        "correct ansible_python_interpreter. (attempted {2})".format(
+                            sys.executable,
+                            sys.version.replace('\n', ''),
+                            candidate_interpreters),
                     results=[],
                     cmd='dnf install -y {0}'.format(package),
                     rc=rc,
